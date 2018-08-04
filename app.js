5fc74a91650c0fde90b00b495a3b348fea4ad96f
Gregor Cox <gregorcox@gmail.com>
2018-08-04 15:49:27 +0100

    adds javascript to add new form inputs to a list

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  const form = document.querySelector('#form');
+  const countryList = document.createElement("ul");
+  const countryListDiv = document.querySelector("#country-list");
+  countryListDiv.appendChild(countryList);
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputCountry = event.target.country.value;
+    const inputContinent = event.target.continent.value;
+    const inputVisited = event.target.visited.value;
+
+    const countryListItem = document.createElement("li");
+    countryListItem.textContent = `Country: ${inputCountry}, Continent: ${inputContinent}, Visted? ${inputVisited}`;
+
+    countryList.appendChild(countryListItem);
+    form.reset();
+
+
+  })
+
+
+
+})
